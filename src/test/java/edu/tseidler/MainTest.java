8938c85a0545d14fb63b0edc3af5b8c1001934cb
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-06-20 12:55:28 +0200

    failing test

---
+++ src/test/java/edu/tseidler/MainTest.java
@@ -0,0 +1,12 @@
+package edu.tseidler;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MainTest {
+
+    @Test
+    public void failingTest() {
+        Assert.assertTrue(false, "this is supposed to fail");
+    }
+}
