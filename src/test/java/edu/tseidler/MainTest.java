88d25f0a96eb8f39c57c21d50a4e33a05b59bdc6
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-06-20 17:42:27 +0200

    addition test

--- src/test/java/edu/tseidler/MainTest.java
+++ src/test/java/edu/tseidler/MainTest.java
@@ -1,33 +1,23 @@
 package edu.tseidler;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class MainTest {
+    private Main main = new Main();
 
-    @Test
-    public void passingTest() {
-        Assert.assertTrue(true, "this is supposed to fail");
+    @DataProvider
+    private static Object[][] dataForAddition() {
+        return new Object[][]{
+                {1, 1, 2},
+                {2, 5, 7},
+                {2, -5, -3},
+        };
     }
 
-    @Test
-    public void passingTest2() {
-        Assert.assertEquals(1, 1);
+    @Test(dataProvider = "dataForAddition")
+    public void shouldAddNumbers(int a, int b, int sum) {
+        Assert.assertEquals(main.add(a, b), sum);
     }
-
-    @Test
-    public void passingTest3() {
-        Assert.assertEquals(2, 2);
-    }
-
-    @Test
-    public void passingTest4() {
-        Assert.assertEquals(2, 2);
-    }
-
-//    @Test
-//    public void failingTest() {
-//        Assert.assertEquals(3, 5);
-//    }
-
 }
